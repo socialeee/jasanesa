cae45ea33e7951764e39838a1d121795835dc8f0
adi <adiyustiawan@unesa.ac.id>
2023-03-29 14:43:13 +0700

    perbaikan nama model

--- app/Http/Controllers/User/ConsultantController.php
+++ app/Http/Controllers/User/ConsultantController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Models\bidang;
+use App\Models\Bidang;
 use App\Models\Consultant;
 use Illuminate\Http\Request;
 
